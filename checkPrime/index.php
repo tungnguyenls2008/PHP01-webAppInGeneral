655a3f01f1f0152e0dffd9d62c27a94096abff8b
root <root@localhost.localdomain>
2020-04-03 23:17:15 +0700

    homework

--- checkPrime/index.php
+++ checkPrime/index.php
@@ -1,11 +1,9 @@
 <?php
 function isPrimeNumber($n)
 {
-
     if ($n < 2) {
         return false;
     }
-
     for ($i = 2; $i <= sqrt($n); $i++) {
         if ($n % $i == 0) {
             return false;
