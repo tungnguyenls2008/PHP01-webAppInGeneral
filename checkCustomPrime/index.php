e8d148d2386026580bea56ca2ec1f2ebec440c7b
root <root@localhost.localdomain>
2020-04-03 23:42:03 +0700

    homework

--- checkCustomPrime/index.php
+++ checkCustomPrime/index.php
@@ -1,30 +1,4 @@
 <?php
-/*$amount = $_REQUEST['amount'];
-$count = 0;
-function isPrimeNumber($n)
-{
-    if ($n < 2) {
-        return false;
-    }
-    for ($i = 2; $i <= sqrt($n); $i++) {
-        if ($n % $i == 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
-echo("Prime numbers that's smaller than 100: <br>");
-for ($i = 0; $i < 100; $i++) {
-    for ($count = 0; $count < $amount; $count++) {
-        if (isPrimeNumber($i)) {
-            echo($i . "; ");
-        }
-    }
-}
-
-*/ ?>
-<?php
 if (isset($_POST["submit"])) {
     $amount = $_REQUEST['amount'];
     $count1 = 0;
