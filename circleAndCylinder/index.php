ece98e4c9c729cf6188c701ecc352ea5e8ef4760
root <root@localhost.localdomain>
2020-04-10 16:08:08 +0700

    updated

--- circleAndCylinder/index.php
+++ circleAndCylinder/index.php
@@ -1,1 +1,8 @@
 <?php
+include 'class/Circle.php';
+include "class/Cylinder.php";
+$circle= new Circle(4,'red');
+$cylinder=new Cylinder(5,'blue',7);
+$circle->toString();
+echo '<br>';
+$cylinder->toString();
